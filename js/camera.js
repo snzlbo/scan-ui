30ae76752fde4d984d4746943041cea158d093af
Sainzolboo Anujin <71211928+snzlbo@users.noreply.github.com>
2024-04-23 11:30:41 +0800

    Add files via upload

---
+++ js/camera.js
@@ -0,0 +1,36 @@
+function camera() {
+  const video = document.getElementById("video");
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+  const facingMode = "environment" || "user"; // "environment"
+
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: facingMode,
+      width: { ideal: window.innerWidth },
+      height: { ideal: window.innerHeight }
+    },
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      video.setAttribute("autoplay", "");
+      video.setAttribute("muted", "");
+      video.setAttribute("playsinline", "");
+      video.width = window.innerWidth;
+      video.style.height = 'calc(var(--app-height) - env(safe-area-inset-top) - env(safe-area-inset-bottom) - 60px)'
+      video.srcObject = stream;
+      try {
+        appHeight()
+      } catch (error) {
+        
+      }
+    })
+    .catch(() => {
+      // device don't support camera → write link function here
+    });
+}
